6fa8ebc3523df8aad8e9b41eeb3da2c360ec0d36
Nic <nicarasmussen@gmail.com>
2019-09-12 09:56:57 -0600

    Add files via upload

---
+++ Utilities.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class Utilities 
+{
+    public static string ProcessText(string input)
+    {
+        //your code goes here:
+        //Determine whether user has input a word or a number,
+        //Return the string 'word' if user enters a word (includes 'ab.23cd')
+        //Return the string 'number if user enters a number (includes 1.3)
+        double number;
+         string[] separateStrings = input.Split(' ');
+         int wordCount = 0;
+         int numCount = 0;
+        double avg = 0;
+        double letterCount = 0;
+
+        foreach (var word in separateStrings)
+         {
+            try
+             {
+
+                 number = double.Parse(word);
+                
+                 numCount++;
+                
+             }
+             catch
+             {
+                 wordCount++;
+                letterCount += word.Length;
+            }
+
+
+         }
+        avg = letterCount / wordCount;
+        return $"Average # of Letters per word: {avg}";
+
+         //return ($"Words: {wordCount} Numbers: {numCount}");
+         
+         /*
+        try
+        {
+            number = double.Parse(input);
+
+            return "number";
+        }
+        catch
+        {
+            return "word";
+        }
+        */
+        //return input;
+    }
+}
